46a2cf99419772f83dd252cc07397669ab6fe8bd
ZhangZidong <2018zhangzd@gmail.com>
2019-01-05 15:01:10 -0500

    Change folder name and make a new repository

--- EE553_CPP/main.cpp
+++ EE553_CPP/main.cpp
@@ -2,14 +2,31 @@
 //  main.cpp
 //  EE553_CPP
 //
-//  Created by Zidong Zhang on 1/5/19.
-//  Copyright © 2019 Zidong Zhang. All rights reserved.
+//  Created by Zidong Zhang on 1/1/19.
+//  Copyright © 2019 张子洞. All rights reserved.
 //
 
 #include <iostream>
+#include "session2.h"
+#include "session3.h"
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// declare function first
+void session1();
+void session2();
+void session3();
+// main function
+int main() {
+    session1();
+    session2();
+    session3();
     return 0;
+}
+// define own function
+void session1(){
+    cout << "hello world" << endl;
+    cout << 2000 * 2000 << '\n';
+    cout << 2000000L * 2000000 << '\n';
+    //    cout << 2000000L * 2000000L << '\n';
+    //    cout << 2000000LL * 2000000 << '\n'; // type promotion
 }

bda584a4f1faf8b8ffdd3f4dbabe5fda926ac1c3
ZhangZidong <2018zhangzd@gmail.com>
2019-01-05 14:55:18 -0500

    Initial Commit

---
+++ EE553_CPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EE553_CPP
+//
+//  Created by Zidong Zhang on 1/5/19.
+//  Copyright © 2019 Zidong Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
